28f01e47884a8b4eac6a98742aceb46e888b62d7
Epistol <mika63270@live.fr>
2016-11-27 21:25:38 +0100

    'tout'

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,26 @@
+<div class="ui inverted vertical footer segment">
+    <div class="ui center aligned container">
+        <div class="ui stackable inverted grid">
+            <div class="five wide column">
+                <div class="active item">CDG</div>
+            </div>
+            <div class="six wide column">
+                <div class="ui inverted celled horizontal link list ">
+                    <a href="/admin" class="item">Admin</a>
+                    <a href="/contact" class="item">Contact</a>
+                    <a href="/modules/about" class="item">A propos</a>
+                    <a href="/modules/faq" class="item">FAQ</a>
+                    <a href="/modules/cgu" class="item">CGU</a>
+                    <a href="javascript:tarteaucitron.userInterface.openPanel();" class="item">Gestion des cookies</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    The current UNIX timestamp is {{ time() }}.
+
+
+</div>
+
+
+
